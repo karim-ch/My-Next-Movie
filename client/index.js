df512c57f5d25a4d00ffa532ae9493e47581f113
Chaâri Karim <chaari.karim.plus@gmail.com>
2019-11-25 16:47:44 +0100

    Added react

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/style.css';
+const App = () => {
+  return <div>Hello World</div>;
+};
+ReactDOM.render(<App />, document.querySelector('#root'));
