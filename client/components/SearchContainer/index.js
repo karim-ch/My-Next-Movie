6ec7a1671b2b201d5588ef4449c01c95ed18bf2f
Chaâri Karim <chaari.karim.plus@gmail.com>
2019-12-11 17:33:48 +0100

    added style to search page

--- client/components/SearchContainer/index.js
+++ client/components/SearchContainer/index.js
@@ -22,7 +22,10 @@ export default function SearchContainer(props) {
             <div>
               <div className="search-filter-card">
                 <div className="search-section" />
-                <p className="search-title-right">Enter your search</p>
+                <p className="search-title-right">
+                  {' '}
+                  Search for a Movie/Tv show...
+                </p>
                 <input
                   className="form-control"
                   onChange={event => {
@@ -35,11 +38,40 @@ export default function SearchContainer(props) {
                   }}
                   value={input}
                 />
+                <nav style={{ paddingTop: '30px' }} aria-label="...">
+                  <ul className="pagination">
+                    <li className="page-item disabled">
+                      <a className="page-link" href="#" tabIndex="-1">
+                        Previous
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        1
+                      </a>
+                    </li>
+                    <li className="page-item active">
+                      <a className="page-link" href="#">
+                        2 <span className="sr-only">(current)</span>
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        3
+                      </a>
+                    </li>
+                    <li className="page-item">
+                      <a className="page-link" href="#">
+                        Next
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
               </div>
 
               <div className="search-filter-card">
                 <div className="search-section" />
-                <p className="search-title-right">Filter By</p>
+                <p className="search-title-right">Filter search by</p>
                 <input
                   className="form-control"
                   onChange={event => {
@@ -78,7 +110,9 @@ export default function SearchContainer(props) {
           <div>
             <div className="search-filter-card">
               <div className="search-section" />
-              <p className="search-title-right">Enter your search</p>
+              <p className="search-title-right">
+                Search for a Movie/Tv show...
+              </p>
               <input
                 className="form-control"
                 onChange={event => {
@@ -91,32 +125,51 @@ export default function SearchContainer(props) {
                 }}
                 value={input}
               />
+
+              <nav style={{ paddingTop: '30px' }} aria-label="...">
+                <ul className="pagination">
+                  <li className="page-item disabled">
+                    <a className="page-link" href="#" tabIndex="-1">
+                      Previous
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a className="page-link" href="#">
+                      1
+                    </a>
+                  </li>
+                  <li className="page-item active">
+                    <a className="page-link" href="#">
+                      2 <span className="sr-only">(current)</span>
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a className="page-link" href="#">
+                      3
+                    </a>
+                  </li>
+                  <li className="page-item">
+                    <a className="page-link" href="#">
+                      Next
+                    </a>
+                  </li>
+                </ul>
+              </nav>
             </div>
 
             <div className="search-filter-card">
               <div className="search-section" />
-              <p className="search-title-right">Filter By</p>
-              <input
-                className="form-control"
-                onChange={event => {
-                  if (event.target.value.length > 1) {
-                    setSearch(event.target.value);
-                    setInput(event.target.value);
-                  } else {
-                    setInput(event.target.value);
-                  }
-                }}
-                value={input}
-              />
+              <p className="search-title-right">Filter search by</p>
             </div>
           </div>
         </div>
 
         {/********************DIV 2****************************** */}
+
         <div className="col-lg-8 col-md-6 col-sm-6">
           <div className="row">
             <div className="col-12 search-result-count">
-              <div className="search-title">{`${data.searchMovies.length}+ Search Result for : ${search}`}</div>
+              <div className="search-title">{`${data.searchMovies.length}Search Result for : ${search}`}</div>
             </div>
             {data.searchMovies.map((movie, i) => (
               <div key={i} className="col-lg-4 col-md-6 col-sm-6 mb-4">
