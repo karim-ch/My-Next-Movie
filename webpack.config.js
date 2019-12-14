6402d858e11dac5c2b51899f823b2ffbf40c641a
Chaâri Karim <chaari.karim.plus@gmail.com>
2019-12-15 00:18:47 +0100

    modified package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,3 @@
-// import webpack from 'webpack';
-// import HtmlWebpackPlugin from 'html-webpack-plugin';
-// import LiveReloadPlugin from 'webpack-livereload-plugin';
-// import path from 'path';
-// const buildPath = path.join(__dirname, 'dist');
-
-// export default {
-//   entry: './client/index.js',
-//   output: {
-//     path: buildPath,
-//     filename: 'bundle.js'
-//   },
-//   module: {
-//     rules: [
-//       {
-//         use: 'babel-loader',
-//         test: /\.js$/,
-//         exclude: /node_modules/
-//       },
-//       {
-//         use: ['style-loader', 'css-loader'],
-//         test: /\.css$/
-//       },
-//       {
-//         test: /\.(png|j?g|svg|gif)?$/,
-//         use: 'file-loader'
-//       }
-//     ]
-//   },
-//   devServer: {
-//     historyApiFallback: true
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: 'client/index.html'
-//     }),
-//     new LiveReloadPlugin()
-//     // new webpack.optimize.UglifyJsPlugin({
-//     //   //..
-//     // })
-//   ]
-// };
-
 import webpack from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import LiveReloadPlugin from 'webpack-livereload-plugin';
@@ -73,6 +30,7 @@ export default {
     new HtmlWebpackPlugin({
       template: 'client/index.html'
     }),
-    new LiveReloadPlugin()
+    new LiveReloadPlugin(),
+    new webpack.optimize.UglifyJsPlugin({})
   ]
 };
